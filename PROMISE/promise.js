28818cc649f88df99d48a8c6a76e80852f874fd7
Chibundu <ndubest56@gmail.com>
2023-09-18 12:36:27 +0100

    more on promises

--- PROMISE/promise.js
+++ PROMISE/promise.js
@@ -1,38 +1,46 @@
 const promise = new Promise((resolve, reject) => {
-    let random = Math.ceil(Math.random() * 4)
-    console.log(random)
-    if (random === 3 || random === 2){
-       resolve("solution was reached") 
-    } else {
-        reject("solution was not reached")
-    }   
-})
-console.log(promise)
-promise.then(data => console.log(data))
-.catch(data => console.log(data))
+  let random = Math.ceil(Math.random() * 4);
+  console.log(random);
+  if (random === 3 || random === 2) {
+    resolve("solution was reached");
+  } else {
+    reject("solution was not reached");
+  }
+});
+console.log(promise);
+promise.then((data) => console.log(data)).catch((data) => console.log(data));
 
 //more on promises
-let btn = document.querySelector(".btn")
+let btn = document.querySelector(".btn");
 
 btn.addEventListener("click", () => {
-    addColor(".mbu", 1000, "red")
-    .then(() => addColor(".abu", 2000, "blue")
-    .then(() => addColor(".atu", 2000, "brown")
-    .then(() => addColor(".anu", 2000, "purple"))))
-    .catch((err) => console.log(err))
-})
+  addColor(".mbu", 1000, "red")
+    .then(() =>
+      addColor(".abu", 2000, "blue").then(() =>
+        addColor(".atu", 2000, "brown").then(() =>
+          addColor(".anu", 2000, "purple")
+        )
+      )
+    )
+    .catch((err) => console.log(err));
+});
 
-let button = document.
+let button = document.querySelector(".but");
+button.addEventListener("click", () => {
+  addColor(".one", 1000, "#0063F8")
+  .then(() => addColor(".two", 2000, "#F60492")
+  .then(() => addColor(".three", 2000, "#EB9705")
+  .then(() => addColor(".four", 3000, "#9F7BFC")))).catch((err) => console.log(err));
+});
 
 const addColor = (selector, time, color) => {
-    //console.log("add color")
-    let element = document.querySelector(selector)
-    return new Promise((resolve, reject) => {
-        if (element) {
-            setTimeout(()=>{
-                element.style.color = color
-                resolve()
-            }, time)
-        } else reject(`There is no selector named: "${selector}"`)
-    })
-}
+  let element = document.querySelector(selector);
+  return new Promise((resolve, reject) => {
+    if (element) {
+      setTimeout(() => {
+        element.style.color = color;
+        resolve();
+      }, time);
+    } else reject(`There is no selector named: "${selector}"`);
+  });
+};
